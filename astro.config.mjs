453d5ca2c0885ea981abdeaef2d23f0ef402ba61
Alonso91CL <alonso91cl@gmail.com>
2024-07-27 20:05:36 -0600

    Configurar despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://alonso91cl.github.io',
+  site: 'http://alonso91cl.github.io/screen-recorder/',
   base: 'screen-recorder',
+  output: 'static',
+  outDir: 'dist'
 });
